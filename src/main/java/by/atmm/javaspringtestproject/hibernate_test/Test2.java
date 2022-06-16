eb2f8b8f336d3b1f0fa3df751f5246be1ae89acf
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-06-16 12:26:07 +0300

    session.save, session.get added

--- src/main/java/by/atmm/javaspringtestproject/hibernate_test/Test2.java
+++ src/main/java/by/atmm/javaspringtestproject/hibernate_test/Test2.java
@@ -10,22 +10,32 @@ import org.hibernate.cfg.Configuration;
  *  @date 2022-06-16
  */
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
 
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
-
         try {
 
-            Employee emp = new Employee("Vasily", "Ivanov", "Sales", 800);
-
             Session session = factory.getCurrentSession();
+
+//            Employee emp = new Employee("Vasily", "Ivanov", "Sales", 800);
+            Employee emp = new Employee("Andrey", "Vasilyev", "IT", 700);
+            //Employee emp = new Employee("Mike", "Andreev", "HR", 600);
+
             session.beginTransaction();
             session.save(emp);
+            System.out.println("Emp after save:" + emp.toString());
+//            session.getTransaction().commit();
+
+            int myId = emp.getId();
+//            session.beginTransaction();
+            Employee employee = session.get(Employee.class, myId);
             session.getTransaction().commit();
+            String empString = "Employee by id: " + myId + " from base: " + employee.toString();
+            System.out.println(empString);
 
         } finally {
             factory.close();
