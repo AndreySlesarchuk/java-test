7cab4ab941dc16df89e7d4d2c0123ab7662985cc
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-06-21 12:04:28 +0300

    info changed

---
+++ src/main/java/by/atmm/javaspringtestproject/jmm/Container.java
@@ -0,0 +1,19 @@
+package by.atmm.javaspringtestproject.jmm;
+
+/**
+ *  @author Andrey Slesarchuk
+ *  @date 2022-06-21
+ *
+ */
+
+public class Container {
+    private String initial = "A";
+
+    public String getInitial() {
+        return initial;
+    }
+
+    public void setInitial(String initial) {
+        this.initial = initial;
+    }
+}
