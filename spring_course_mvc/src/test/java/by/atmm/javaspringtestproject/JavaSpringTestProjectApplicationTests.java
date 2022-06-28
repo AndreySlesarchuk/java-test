b9288a29525c63a92d76ac7760dae9ff298dfed7
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-06-28 22:01:13 +0300

    code refactoring

--- spring_course_mvc/src/test/java/by/atmm/javaspringtestproject/JavaSpringTestProjectApplicationTests.java
+++ spring_course_mvc/src/test/java/by/atmm/javaspringtestproject/JavaSpringTestProjectApplicationTests.java
@@ -1,11 +1,11 @@
-package by.atmm.java;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest class javaApplicationTests {
-
-  @Test void contextLoads() {
-  }
-
-}
+//package by.atmm.java;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest class javaApplicationTests {
+//
+//  @Test void contextLoads() {
+//  }
+//
+//}
