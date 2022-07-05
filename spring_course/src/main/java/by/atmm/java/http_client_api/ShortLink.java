c84560ae6aa88a6abe69676184f5f05dd22ae160
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-07-05 19:03:17 +0300

    ShortLink.java refactored;

--- spring_course/src/main/java/by/atmm/java/http_client_api/ShortLink.java
+++ spring_course/src/main/java/by/atmm/java/http_client_api/ShortLink.java
@@ -1,64 +1,47 @@
 package by.atmm.java.http_client_api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.java.Log;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+@Log
 public class ShortLink {
 
     public static final String URI_STRING = "https://goo.su/api/links/create";
 
 
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    public static void main(String[] args) {
         String linkStr = "https://ryanharrison.co.uk/2018/06/15/make-http-requests-kotlin.html";
         ShortLink sl = new ShortLink();
-        Map<String,String> jsonData = new HashMap<>();
+        Map<String, String> jsonData = new HashMap<>();
         jsonData.put("url", linkStr);
 
-        String shortLink = sl.getShortLink(new URI(URI_STRING), jsonData);
+        String shortLink = sl.getShortLink(URI_STRING, jsonData);
         System.out.println(shortLink);
     }
 
-    public String getShortLink(URI uri, Map<String, String> map) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody = null;
+    public String getShortLink(String uri, Map<String, String> map) {
+        Map<String, Object> responseMap = null;
         try {
-            requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-        } catch (JsonProcessingException e) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+            HttpRequest request = HttpRequest.newBuilder(new URI(uri))
+                    .header("Content-Type", "application/json")
+                    .header("x-goo-api-token", "qMMJkTBBbMLUi5IAgYLm8jbF2y8iD2nULA0xc1UcQ7oxtDL4htbJTeR56JWm")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody)).build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            responseMap = objectMapper.readValue(response.body(), Map.class);
+        } catch (Exception e) {
             throw new IllegalArgumentException("Short link: data to work is not correct " + map.toString());
         }
 
-        HttpRequest request = HttpRequest.newBuilder(uri)
-                .header("Content-Type", "application/json")
-                .header("x-goo-api-token", "qMMJkTBBbMLUi5IAgYLm8jbF2y8iD2nULA0xc1UcQ7oxtDL4htbJTeR56JWm")
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                .build();
-
-        HttpResponse response = null;
-        try {
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Short link: data to work is not correct " + map.toString());
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        Map<String, String> responseMap = new HashMap<>();
-        try {
-             responseMap =objectMapper.readValue(response.body().toString(), Map.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (response.statusCode() == 200) {
+        if (responseMap.get("successful").equals(true)) {
             return responseMap.get("short_url").toString();
         } else {
             return "";
