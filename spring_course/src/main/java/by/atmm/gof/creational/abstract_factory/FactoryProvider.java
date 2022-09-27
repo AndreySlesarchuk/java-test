bb91616881468575ce2d12434a403c1812509c2b
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-09-27 12:54:02 +0300

    FactoryProvider.java changed

--- spring_course/src/main/java/by/atmm/gof/creational/abstract_factory/FactoryProvider.java
+++ spring_course/src/main/java/by/atmm/gof/creational/abstract_factory/FactoryProvider.java
@@ -1,6 +1,11 @@
 package by.atmm.gof.creational.abstract_factory;
 
 public class FactoryProvider {
+
+    private FactoryProvider() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static AbstractFactory getFactory(String choice){
 
         if("Toy".equalsIgnoreCase(choice)){
