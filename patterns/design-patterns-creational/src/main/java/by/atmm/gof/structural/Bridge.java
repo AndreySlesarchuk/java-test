d3664a472dfca2c4de0cc74c99912cf4e38a3203
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-10-03 16:43:05 +0300

    Bridge.java - structural pattern added

---
+++ patterns/design-patterns-creational/src/main/java/by/atmm/gof/structural/Bridge.java
@@ -0,0 +1,64 @@
+package by.atmm.gof.structural;
+
+public class Bridge {
+    public static void main(String[] args) {
+        Shape square = new Square(new Red()); //a square with red color
+        System.out.println(square.draw());
+
+        Shape triangle = new Triangle(new Green()); //a triangle with green color
+        System.out.println(triangle.draw());
+    }
+}
+
+// Shapes
+abstract class Shape {
+    protected Color color;
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    abstract public String draw();
+}
+
+class Square extends Shape {
+    public Square(Color color) {
+        super(color);
+    }
+
+    @Override
+    public String draw() {
+        return "Square drawn. " + color.fill();
+    }
+}
+
+class Triangle extends Shape {
+    public Triangle(Color color) {
+        super(color);
+    }
+
+    @Override
+    public String draw() {
+        return "Triangle drawn. " + color.fill();
+    }
+}
+
+// Colors
+interface Color {
+    String fill();
+}
+
+class Green implements Color {
+    @Override
+    public String fill() {
+        return "Color is Green";
+    }
+}
+
+class Red implements Color {
+    @Override
+    public String fill() {
+        return "Color is Red";
+    }
+}
+
