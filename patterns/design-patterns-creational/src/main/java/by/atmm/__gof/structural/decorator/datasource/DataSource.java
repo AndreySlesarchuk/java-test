21d241bce0925a859817d27931ab27324aacecf1
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-10-05 18:12:34 +0300

    DataSourceDecorator.java.java - structural pattern "Decorator" added

---
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__gof/structural/decorator/datasource/DataSource.java
@@ -0,0 +1,9 @@
+package by.atmm.__gof.structural.decorator.datasource;
+
+public interface DataSource {
+
+    void writeData(String data);
+
+    String readData();
+
+}
---
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__gof/structural/decorator/datasource/DataSourceDecorator.java
@@ -0,0 +1,19 @@
+package by.atmm.__gof.structural.decorator.datasource;
+
+public class DataSourceDecorator implements DataSource {
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
