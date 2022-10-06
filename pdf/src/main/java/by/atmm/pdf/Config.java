abded8aaa9983f3d2e3391d6e030583ff3cd77d2
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-10-06 16:30:32 +0300

    module pdf added

---
+++ pdf/src/main/java/by/atmm/pdf/Config.java
@@ -0,0 +1,9 @@
+package by.atmm.pdf;
+
+public class Config {
+    public static final String INPUT_DIR = "d:/__resources/input/";
+    public static final String OUTPUT_DIR = "d:/__resources/output/";
+
+    private Config() {
+    }
+}
