966c16c04edd6b8e712d87cb68d68dc907c73949
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-10-08 22:24:42 +0300

    solid: start off

---
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__solid/singleresponsibility/BadBook.java
@@ -0,0 +1,26 @@
+package by.atmm.__solid.singleresponsibility;
+
+public class BadBook {
+
+    private String name;
+    private String author;
+    private String text;
+
+    // constructor, getters and setters
+
+    // methods that directly relate to the book properties
+    public String replaceWordInText(String word, String replacementWord){
+        return text.replaceAll(word, replacementWord);
+    }
+
+    public boolean isWordInText(String word){
+        return text.contains(word);
+    }
+
+    // methods for outputting text to console - should this really be here?
+    void printTextToConsole(){
+        // our code for formatting and printing the text
+    }
+
+
+}
