e5521c87bce396ee48db29f6ccf36e1d9e912b67
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-11-02 16:03:50 +0300

    ActiveMQ Publisher.java added

---
+++ spring_course/src/main/java/by/atmm/java/activemq/Publisher.java
@@ -0,0 +1,35 @@
+package by.atmm.java.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class Publisher {
+
+    public static void main(String[] args) {
+
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin"
+                , "tcp://localhost:61616");
+
+        try {
+            Connection connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("demo");
+
+            TextMessage textMessage = session.createTextMessage("First message");
+
+            MessageProducer producer = session.createProducer(destination);
+            producer.send(textMessage);
+
+            System.out.println("Message Published");
+
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
