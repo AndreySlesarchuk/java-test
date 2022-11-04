ebfc44d89a291e5fb2327ac2883cd556dd27314b
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-11-04 22:49:10 +0300

    code refactoring

--- patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/visitor/animal/EatVisitor.java
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/visitor/animal/EatVisitor.java
@@ -4,18 +4,20 @@ package by.atmm.__gof.behavioral.visitor.animal;
  * Created by Andrey Slesarchuk on 05/25/2020.
  */
 public class EatVisitor implements AnimalVisitor {
-  @Override
-  public void action(Cat cat) {
-    System.out.println("eat fish");
-  }
 
-  @Override
-  public void action(Dog dog) {
-    System.out.println("eat meat");
-  }
+    @Override
+    public void action(Cat cat) {
+        System.out.println("eat fish");
+    }
 
-  @Override
-  public void action(Chicken chicken) {
-    System.out.println("eat corn");
-  }
+    @Override
+    public void action(Dog dog) {
+        System.out.println("eat meat");
+    }
+
+    @Override
+    public void action(Chicken chicken) {
+        System.out.println("eat corn");
+    }
+
 }
