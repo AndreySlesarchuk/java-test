d04d2f63639769d1e57821f6eb19d86046684f8e
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-11-05 21:26:49 +0300

    code refactoring

--- patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/observer/chat/Chat.java
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/observer/chat/Chat.java
@@ -8,34 +8,36 @@ import java.util.logging.Logger;
  * Created by Andrey Slesarchuk on 07/26/2020.
  */
 public class Chat {
-  private static final Logger LOGGER = Logger.getLogger(Chat.class.getName());
 
-  private List messages;
+    private static final Logger LOGGER = Logger.getLogger(Chat.class.getName());
 
-  private List<ChatObserver> observers;
+    private List messages;
 
-  public Chat() {
-    observers = new ArrayList<>();
-    messages = new ArrayList<>();
-  }
+    private List<ChatObserver> observers;
 
-  public void addObserver(ChatObserver observer) {
-    observers.add(observer);
-  }
-
-  public void removeObserver(ChatObserver observer) {
-    observers.remove(observer);
-  }
-
-  public void addMessage(String message) {
-    messages.add(message);
-    LOGGER.info(String.format("Chat get new message: %s", message));
-    notifyObservers(message);
-  }
-
-  private void notifyObservers(String message) {
-    for (ChatObserver observer : observers) {
-      observer.update(message);
+    public Chat() {
+        observers = new ArrayList<>();
+        messages = new ArrayList<>();
     }
-  }
+
+    public void addObserver(ChatObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(ChatObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void addMessage(String message) {
+        messages.add(message);
+        LOGGER.info(String.format("Chat get new message: %s", message));
+        notifyObservers(message);
+    }
+
+    private void notifyObservers(String message) {
+        for (ChatObserver observer : observers) {
+            observer.update(message);
+        }
+    }
+
 }
--- patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/observer/chat/ChatObserver.java
+++ patterns/design-patterns-creational/src/main/java/by/atmm/__gof/behavioral/observer/chat/ChatObserver.java
@@ -4,5 +4,7 @@ package by.atmm.__gof.behavioral.observer.chat;
  * Created by Andrey Slesarchuk on 07/26/2020.
  */
 public interface ChatObserver {
+
   void update(String message);
+
 }
