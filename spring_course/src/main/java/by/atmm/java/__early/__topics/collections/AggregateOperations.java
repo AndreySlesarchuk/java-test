47370a9150908c8090e4725d1449d269ac7a3e11
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-12-16 11:05:22 +0100

    AggregateOperations.java BinaryOperator added

--- spring_course/src/main/java/by/atmm/java/__early/__topics/collections/AggregateOperations.java
+++ spring_course/src/main/java/by/atmm/java/__early/__topics/collections/AggregateOperations.java
@@ -1,13 +1,18 @@
 package by.atmm.java.__early.__topics.collections;
 
 import lombok.Data;
+import org.apache.poi.ss.formula.functions.T;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class AggregateOperations {
 
     public static void main(String[] args) {
+
+        BinaryOperator<Integer> myTest1 = (a, b) -> a*2+b*3;
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         int resultReduce = numbers
                 .stream()
@@ -16,6 +21,9 @@ public class AggregateOperations {
 
         int resultReduceInteger = numbers.stream().reduce(0, Integer::sum);
         System.out.println("Base array: " + numbers + " resultReduceInteger: " + resultReduceInteger); // 21
+
+        int resultReduceMyTest1 = numbers.stream().reduce(0, myTest1);
+        System.out.println("Base array: " + numbers + " resultReduceMyTest1: " + resultReduceMyTest1); // 360
 
         List<String> letters = Arrays.asList("a", "b", "c", "d", "e");
         String resultReduceLetters = letters
@@ -27,6 +35,8 @@ public class AggregateOperations {
         List<Employee> employees = List.of(new Employee("A", 3500, 23), new Employee("B", 2000, 30));
         //employees.stream().reduce(B)
     }
+
+
 
 }
 
