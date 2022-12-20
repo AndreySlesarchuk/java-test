de08a3d1b463125a424e7a025bc846e01781f3f8
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-12-20 21:43:27 +0100

    DataTimeFormat.java changed

--- spring_course/src/main/java/by/atmm/java/__early/__topics/datetime/DataTimeFormat.java
+++ spring_course/src/main/java/by/atmm/java/__early/__topics/datetime/DataTimeFormat.java
@@ -1,6 +1,7 @@
 package by.atmm.java.__early.__topics.datetime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -26,5 +27,13 @@ public class DataTimeFormat {
     System.out.println(
         DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
             .format(zonedDateTime));
+
+    var europeanDatePattern = "dd.MM.yyyy";
+    var europeanDateFormatter = DateTimeFormatter.ofPattern(europeanDatePattern);
+    System.out.println(europeanDateFormatter.format(LocalDate.of(2022, 2, 22)));
+
+    var fileDatePattern = "ddMMyyyykkmm";
+    var fileDateFormatter = DateTimeFormatter.ofPattern(fileDatePattern);
+    System.out.println(fileDateFormatter.format(LocalDateTime.now()));
   }
 }
