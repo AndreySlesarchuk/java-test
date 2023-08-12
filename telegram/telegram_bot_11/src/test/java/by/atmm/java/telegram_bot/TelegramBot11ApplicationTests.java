535d042ab6bc953cc378ffd736793d411c97249c
3237573 <3237573@gmail.com>
2023-08-12 22:57:59 +0300

    Telegram bot, java version 11 with added

---
+++ telegram/telegram_bot_11/src/test/java/by/atmm/java/telegram_bot/TelegramBot11ApplicationTests.java
@@ -0,0 +1,14 @@
+package by.atmm.java.telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBot11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// next step
+	}
+
+}
