b59d4be59bebb2113b45628fc58bde0e5fc5d0c6
3237573 <96978266+3237573@users.noreply.github.com>
2024-05-15 22:24:20 +0300

    refactor: refactor code

--- spring_course/src/main/java/by/atmm/java/__topics/activemq/topics/Consumer.java
+++ spring_course/src/main/java/by/atmm/java/__topics/activemq/topics/Consumer.java
@@ -6,37 +6,37 @@ import javax.jms.*;
 
 public class Consumer {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin"
-                , "tcp://localhost:61616");
+    ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
 
-        try {
-            Connection connection = factory.createConnection();
-            connection.setClientID("1");
-            connection.start();
+    try {
+      Connection connection = factory.createConnection();
+      connection.setClientID("1");
+      connection.start();
 
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-            Topic topic = session.createTopic("Demo-topic");
+      Topic topic = session.createTopic("Demo-topic");
 
-            MessageConsumer consumer = session.createDurableSubscriber(topic, "Consumer-1");
-            consumer.setMessageListener(new MessageListener() {
-                @Override
-                public void onMessage(Message message) {
-                    TextMessage textMessage = (TextMessage) message;
-                    try {
-                        System.out.println(textMessage.getText());
-                    } catch (JMSException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } catch (JMSException e) {
+      MessageConsumer consumer = session.createDurableSubscriber(topic, "Consumer-1");
+      consumer.setMessageListener(new MessageListener() {
+        @Override
+        public void onMessage(Message message) {
+          TextMessage textMessage = (TextMessage) message;
+          try {
+            System.out.println(textMessage.getText());
+          }
+          catch (JMSException e) {
             e.printStackTrace();
+          }
         }
+      });
     }
-
+    catch (JMSException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
 
--- spring_course/src/main/java/by/atmm/java/__topics/activemq/topics/Publisher.java
+++ spring_course/src/main/java/by/atmm/java/__topics/activemq/topics/Publisher.java
@@ -6,30 +6,29 @@ import javax.jms.*;
 
 public class Publisher {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin"
-                , "tcp://localhost:61616");
+    ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
 
-        try {
-            Connection connection = factory.createConnection();
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createTopic("Demo-Topic");
+    try {
+      Connection connection = factory.createConnection();
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Destination destination = session.createTopic("Demo-Topic");
 
-            TextMessage textMessage = session.createTextMessage("Message for topic 2");
+      TextMessage textMessage = session.createTextMessage("Message for topic 2");
 
-            MessageProducer producer = session.createProducer(destination);
-            producer.send(textMessage);
+      MessageProducer producer = session.createProducer(destination);
+      producer.send(textMessage);
 
-            System.out.println("Message published");
+      System.out.println("Message published");
 
-            session.close();
-            connection.close();
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }
+      session.close();
+      connection.close();
     }
-
+    catch (JMSException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
 
