66b2ed619fc7ee163b8e07fa98a52e39d584a248
3237573 <96978266+3237573@users.noreply.github.com>
2024-05-20 13:37:31 +0300

    feat(lambda): add LambdaFunction.java

--- spring_course/src/main/java/by/atmm/java/__topics/lambda/Lambda.java
+++ spring_course/src/main/java/by/atmm/java/__topics/lambda/Lambda.java
@@ -4,23 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-//        Java Developers Community. Follow Deepak Gupta for more such content.
-//        What will be the output of the following code?
-//        1, 3, 5
-//        8%
-//        2, 4
-//        55%
-//        1, 2, 3, 4, 5
-//        12%
-//        None of the above
-//        26%
-//        490 votes
-
-
 public class Lambda {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
 
         Predicate<Integer> predicate = x -> {
             int count = 0;
